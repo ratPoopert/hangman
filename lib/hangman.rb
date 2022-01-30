e386a3b17c1ee34e72d3eaf8942b79a0fa3ead54
ratPoopert <50669669+ratPoopert@users.noreply.github.com>
2022-01-30 14:20:04 +0000

    Implement basic functionality

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -11,4 +11,48 @@ def create_word_list
   JSON.parse(word_list_file.read)
 end
 
+def display_correct_guesses
+  display = []
+  WORD.split('').map do |char|
+    if $correct_guesses.include?(char)
+      display.push(char)
+    else
+      display.push('_')
+    end
+  end
+   
+  "Correct guesses: '#{display.join(' ')}.'"
+end
+
+def display_incorrect_guesses
+  "Incorrect guesses: '#{$incorrect_guesses.join(', ')}.'"
+end
+
+def word_guessed?
+  WORD.chars.all? { |char| $correct_guesses.include?(char)}
+end
+
 word_list = create_word_list
+WORD = word_list.sample
+puts "The word for this game is '#{WORD}'."
+
+$correct_guesses = []
+
+$incorrect_guesses = []
+
+until word_guessed? || $incorrect_guesses.length == 6
+  puts "Guesses remaining: #{6 - $incorrect_guesses.length}."
+  puts "Please enter a letter:"
+  guess = gets.chomp.downcase
+  
+  if WORD.include?(guess)
+    $correct_guesses.push(guess)
+  else
+    $incorrect_guesses.push(guess)
+  end
+  
+  puts display_correct_guesses
+  puts display_incorrect_guesses
+  p word_guessed?
+end
+
